361cbbb34e8ec133890595395829b3eefa461c66
procodestechnologies <procodes41@gmail.com>
2026-06-19 23:30:03 +0300

    initial commit

---
+++ resources/views/components/about.blade.php
@@ -0,0 +1,57 @@
+<div class="container-fluid about py-5">
+            <div class="container py-5">
+                <div class="row g-5 align-items-center">
+                    <div class="col-xl-6 wow fadeInLeft" data-wow-delay="0.1s">
+                        <div class="about-item-image d-flex">
+                            <img src="{{ asset('frontend/img/about.jpg')}}" class="img-1 img-fluid w-50"  alt="">
+                            <img src="{{ asset('frontend/img/about-3.jpg')}}" class="img-2 img-fluid w-50"  alt="">
+                            <div class="about-item-image-content">
+                                <img src="{{ asset('frontend/img/about-1.png')}}" class="img-fluid w-100 h-100" style="object-fit: cover;" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.1s">
+                        <div class="about-item-content">
+                            <p class="text-uppercase text-secondary fs-5 mb-0">WE ARE CONSTRUCTION COMPANY</p>
+                            <h2 class="display-4 text-capitalize mb-3">Making your vision come true at the basics.</h2>
+                            <p class="mb-4 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            </p>
+                            <div class="pb-4 mb-4 border-bottom">
+                                <div class="row g-4">
+                                    <div class="col-lg-4">
+                                        <div class="about-item-content-img">
+                                            <img src="{{ asset('frontend/img/about-2.jpg')}}" class="img-fluid w-100" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="d-flex mb-4">
+                                            <div class="text-secondary">
+                                                <i class="fas fa-user-shield fa-3x"></i>
+                                            </div>
+                                            <h4 class="ms-3">Building quality standards</h4>
+                                        </div>
+                                        <div class="d-flex">
+                                            <div class="text-secondary">
+                                                <i class="fas fa-users-cog fa-3x"></i>
+                                            </div>
+                                            <h4 class="ms-3">Certified engineer’s team</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row gy-0 gx-4 justify-content-between pb-4">
+                                <div class="col-lg-6">
+                                    <p class="text-dark"><i class="fas fa-check text-secondary me-1"></i> 100% Satisfaction</p>
+                                    <p class="text-dark"><i class="fas fa-check text-secondary me-1"></i> Trained Emploies</p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <p class="text-dark"><i class="fas fa-check text-secondary me-1"></i> Annual Pass Programs</p>
+                                    <p class="text-dark mb-0"><i class="fas fa-check text-secondary me-1"></i> Flexible and cost effective</p>
+                                </div>
+                            </div>
+                            <a class="btn btn-secondary d-inline-block py-3 px-5 me-2 flex-shrink-0 wow fadeInUp" data-wow-delay="0.1s" href="#">Discover More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,27 @@
+@extends('welcome')
+@section('content')
+        <!-- Carousel Start -->
+        <x-carousel/>
+        <!-- Carousel End -->
+        
+        <!-- About Start -->
+        <x-about/>
+        
+        <!-- About End -->
+        
+        <!-- Features Start -->
+        
+        <x-features/>
+        <!-- Features End -->
+        
+        
+        <!-- Services Start -->
+        <x-services/>
+        
+        <!-- Services End -->
+        
+        
+        <!-- Blog Start -->
+        <x-blog/>       
+        <!-- Blog End -->
+@endsection
