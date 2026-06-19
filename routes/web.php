4d5b7e0d1507a2ef46075fa275b080cfebd04d4a
procodestechnologies <procodes41@gmail.com>
2026-06-19 23:40:59 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'frontend.home')->name('home');
+Route::prefix('/')->group(function () {
+    Route::get('', function () {
+        return view('frontend.home');
+    });
+    Route::get('about', function () {
+        return view('frontend.about');
+    });
+    Route::get('services', function () {
+        return view('frontend.services');
+    });
+    Route::get('contact', function () {
+        return view('frontend.contact');
+    });
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
